2545a2441a90fe8bc11e34890ede4299f680c6e8
Erick Lumunge <lumungep12@gmail.com>
2021-06-22 19:39:03 +0300

    drinks and cocktails

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { Container } from "@material-ui/core";
 import About from "./Pages/About/About";
 import Navbar from "./Components/Navbar/Navbar";
 import Category from "./Components/Categories/Category/Category";
-import Recipe from "./Pages/Recipe/Recipe";
+import Drink from "./Pages/Recipe/Drink";
 import Categories from "./Components/Categories/Categories";
 import Auth from "./Pages/Home/Auth";
 
@@ -17,7 +17,7 @@ const App = () => {
 					<Route path="/about" component={About} />
 					<Route path="/categories" component={Categories} />
 					<Route path="/category" component={Category} />
-					<Route path="/recipe" component={Recipe} />
+					<Route path="/drink" component={Drink} />
 				</Switch>
 			</Container>
 		</BrowserRouter>
