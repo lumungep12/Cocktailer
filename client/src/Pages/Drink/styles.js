9644c752029907bb619ec18d61910e78aeb23f8b
Erick Lumunge <lumungep12@gmail.com>
2021-07-15 14:11:27 +0300

    drink detsila styling

--- client/src/Pages/Drink/styles.js
+++ client/src/Pages/Drink/styles.js
@@ -1,9 +1,13 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles(() => ({
+	main: {
+		height: "100vh",
+		marginTop: "6rem",
+	},
 	title: {
 		textAlign: "center",
-		paddingTop: "1.5rem",
+		paddingBottom: "2rem",
 	},
 	image: {
 		height: 350,
